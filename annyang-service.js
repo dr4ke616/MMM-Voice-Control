15c755104a2df95ae0342cf35917889304ac5b28
dr4ke616 <adamdrakeford@gmail.com>
2016-07-03 13:57:40 +0100

    Basic implementtion of voice control working

---
+++ annyang-service.js
@@ -0,0 +1,70 @@
+function AnnyangService(root) {
+    var service = {};
+
+    // COMMANDS
+    service.commands = {};
+
+    service.mergeRecursive = function(obj1, obj2) {
+        for (var p in obj2) {
+            try {
+                // Property in destination object set; update its value.
+                if ( obj2[p].constructor==Object ) {
+                    obj1[p] = MergeRecursive(obj1[p], obj2[p]);
+                } else {
+                    obj1[p] = obj2[p];
+                }
+            } catch(e) {
+                // Property in destination object not set; create it and set its value.
+                obj1[p] = obj2[p];
+            }
+        }
+        return obj1;
+    }
+
+    service.addCommand = function(phrase, callback) {
+        var command = {};
+
+        // Wrap annyang command in scope apply
+        command[phrase] = function(arg1, arg2) {
+            callback(arg1, arg2);
+        };
+
+        service.mergeRecursive(service.commands, command);
+
+        // Add the commands to annyang
+        annyang.addCommands(service.commands);
+        console.debug('added command "' + phrase + '"', service.commands);
+    };
+
+    service.setLanguage = function(langCode) {
+        annyang.setLanguage(langCode);
+    };
+
+    service.setLanguage(config.language);
+
+    service.start = function(onListeningStarted, interimResult, result, error) {
+        annyang.addCommands(service.commands);
+        annyang.debug(true);
+        // add specified callback functions
+        if (typeof(onListeningStarted) == "function") {
+            annyang.addCallback('start', function(){ onListeningStarted(true); });
+            annyang.addCallback('end', function(data){ console.log("End", data) });
+        };
+        if (typeof(interimResult) == "function") {
+            annyang.addCallback('interimResult', function(data){ interimResult(data); });
+        };
+        if (typeof(result) == "function") {
+            annyang.addCallback('result', function(data){ result(data); });
+        };
+        if (typeof(error) == "function") {
+            annyang.addCallback('error', function(data){ error(data); });
+        };
+        annyang.start();
+    };
+
+    service.abort = function(){
+        annyang.abort();
+    }
+
+    return service;
+}
---
+++ voicecontrol.js
@@ -0,0 +1,82 @@
+Module.register("voicecontrol", {
+
+	// Default module config.
+    defaults: {
+        language: "en",
+        restTimeout: 3000
+    },
+
+    getScripts: function() {
+    	return ['annyang.js', 'annyang-service.js', 'moment.js']
+	},
+
+	// Define required scripts.
+	getStyles: function() {
+		return ["style.css"];
+	},
+
+	// Override dom generator.
+	getDom: function() {
+		var wrapper = document.createElement("div");
+		wrapper.className = "small light";
+		wrapper.innerHTML = this.interimResult;
+		return wrapper;
+	},
+
+    restCommand: function(scope) {
+    	scope.interimResult = "Say Something...";
+    	scope.updateDom();
+    },
+
+	start: function() {
+		Log.info("Starting module: " + this.name);
+
+		// Set locale.
+		moment.locale(config.language);
+
+		this.annyangService = AnnyangService();
+		this.isListening = false;
+		this.interimResult = "Say Something...";
+
+		this.registerCommands();
+		this.startAnnayang();
+	},
+
+	registerCommands: function() {
+		this.annyangService.addCommand('what can I say', function() {
+			Log.info("I ma here±!")
+		})
+	},
+
+	startAnnayang: function() {
+		var self = this
+		var resetCommandTimeout;
+		this.annyangService.start(
+			function(){
+				self.isListening = true;
+			},
+			function(interimResult){
+				self.interimResult = interimResult;
+				window.clearTimeout(resetCommandTimeout);
+				Log.info("Interim result " + self.interimResult)
+				self.updateDom()
+			},
+			function(result){
+				if(typeof result != 'undefined'){
+					self.interimResult = result[0];
+					resetCommandTimeout = window.setTimeout(function() {
+						self.restCommand(self)
+					}, self.config.restTimeout);
+				}
+			},
+			function(error){
+				console.log(error);
+				if(error.error == "network"){
+					self.speechError = "Google Speech Recognizer is down :(";
+					self.annyangService.abort();
+					self.isListening = false;
+				}
+			}
+		);
+	},
+});
